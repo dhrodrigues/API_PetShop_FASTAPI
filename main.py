35c451bee876420bc762d9ea193f5e7c63477886
Diego Rodrigues <diego17ar@hotmail.com>
2022-11-18 14:59:56 -0300

    Atualização

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from fastapi import FastAPI
 from pydantic import BaseModel
 import uvicorn
+from fastapi import FastAPI
+from datetime import datetime
+from typing import Dict
 
 
 app = FastAPI()
 
-accounts = []
+donos = []
 
 class Dono(BaseModel):
     id: int
@@ -15,18 +17,21 @@ class Dono(BaseModel):
     email: str
     telefone: str
 
-class Pet2(BaseModel):
-    id: int
-    nome: str
-    documento: str
-    dataNacimento: str
-    email: str
-    telefone: str
 
 
-@app.get("/home")
-def home():
-    return("Bem vindo a PETSHOP DH")
+
+@app.get("/health")
+def alive() -> Dict[str, datetime]:
+    return {"timestamp": datetime.now()}
+
+@app.post("/cadastro", response_model=bool, tags=["cadastro"])
+def create_login(dono: Dono):
+    donos.append(dono)   
+    return True
+
+@app.get("/accounts", response_model=list, tags=["users"])
+def list_users():
+    return  donos
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8085, reload=True)
